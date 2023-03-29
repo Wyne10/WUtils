c9e6f5ea4a9b42766ed327eb4088c86a0baaf08f
Wyne <officialfitcher@gmail.com>
2023-03-29 21:27:46 +0500

    Added methods for map Changed some log messages Changed some comments

--- storage/src/main/java/me/wyne/wutils/storage/Storage.java
+++ storage/src/main/java/me/wyne/wutils/storage/Storage.java
@@ -4,7 +4,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
-import java.util.HashMap;
+import java.util.Map;
 
 public interface Storage {
 
@@ -22,88 +22,126 @@ public interface Storage {
     void loadData();
 
     /**
-     * Get element from {@link HashMap}. May be used to add extra logic to data query.
-     * HashMap is used because data is often stored as key:value.
-     * @param data {@link HashMap} to get element from
-     * @param key {@link HashMap} key to get element from
-     * @return Element of {@link ValType} from data or null
-     * @param <KeyType> Data {@link HashMap} key type
-     * @param <ValType> Data {@link HashMap} value type
+     * Get element from data {@link Map}. May be used to add extra logic to data query.
+     * Map is used because data is often stored as key:value.
+     * @param data Data {@link Map} to get element from
+     * @param key Data {@link Map} key to get element from
+     * @return Element of {@link ValType} from data {@link Map} or null
+     * @param <KeyType> Data {@link Map} key type
+     * @param <ValType> Data {@link Map} value type
      */
     @Nullable
-    <KeyType, ValType> ValType get(@NotNull final HashMap<KeyType, ValType> data, @NotNull final KeyType key);
+    <KeyType, ValType> ValType get(@NotNull final Map<KeyType, ValType> data, @NotNull final KeyType key);
     /**
-     * Get {@link Collection} from {@link HashMap}. May be used to add extra logic to data query.
-     * HashMap is used because data is often stored as key:value.
-     * @param data {@link HashMap} to get {@link Collection} from
-     * @param key {@link HashMap} key to get {@link Collection} from
-     * @return {@link Collection} from data or empty set
-     * @param <KeyType> Data {@link HashMap} key type
+     * Get {@link Collection} from data {@link Map}. May be used to add extra logic to data query.
+     * Map is used because data is often stored as key:value.
+     * @param data Data {@link Map} to get {@link Collection} from
+     * @param key Data {@link Map} key to get {@link Collection} from
+     * @return {@link Collection} from data {@link Map} or empty set
+     * @param <KeyType> Data {@link Map} key type
      * @param <ValType> {@link Collection} value type
      */
     @NotNull
-    <KeyType, ValType> Collection<ValType> getCollection(@NotNull final HashMap<KeyType, ? extends Collection<ValType>> data, @NotNull final KeyType key);
+    <KeyType, ValType> Collection<ValType> getCollection(@NotNull final Map<KeyType, ? extends Collection<ValType>> data, @NotNull final KeyType key);
 
     /**
-     * Save element to data {@link HashMap} and to storageFile to given path.
+     * Save element to data {@link Map} and to storageFile to given path.
      * <br>Set path as null to save value directly to key in storageFile.
      * <br>Set data as null to save value only to storageFile.
-     * @param data {@link HashMap} to save element to
-     * @param key {@link HashMap} key to save element to
+     * @param data Data {@link Map} to save element to
+     * @param key Data {@link Map} key to save element to
      * @param value Value to save
      * @param path Path in storageFile to save element to
-     * @param <KeyType> Data {@link HashMap} key type
-     * @param <ValType> Data {@link HashMap} value type
+     * @param <KeyType> Data {@link Map} key type
+     * @param <ValType> Data {@link Map} value type
      */
-    <KeyType, ValType> void save(@Nullable HashMap<KeyType, ValType> data, @NotNull final KeyType key, @NotNull final ValType value, @Nullable final String path);
+    <KeyType, ValType> void save(@Nullable Map<KeyType, ValType> data, @NotNull final KeyType key, @NotNull final ValType value, @Nullable final String path);
     /**
-     * Save element to {@link Collection} in data {@link HashMap} and to storageFile to given path.
-     * @param data {@link HashMap} to save {@link Collection} to
-     * @param key {@link HashMap} key to {@link Collection} to save element to
+     * Save element to {@link Collection} in data {@link Map} and to storageFile to given path.
+     * @param data Data {@link Map} to save {@link Collection} to
+     * @param key Data {@link Map} key to {@link Collection} to save element to
      * @param value Value to save
      * @param path Path to {@link com.google.gson.JsonArray} in storageFile to save element to
      * @return True/False if save succeed/failed
-     * @param <KeyType> Data {@link HashMap} key type
+     * @param <KeyType> Data {@link Map} key type
      * @param <ValType> {@link Collection} value type
      * @param <ColType> Type of {@link Collection}
      */
-    <KeyType, ValType, ColType extends Collection<ValType>> boolean saveCollection(@NotNull HashMap<KeyType, ColType> data, @NotNull final KeyType key, @NotNull final ValType value, @NotNull final String path);
+    <KeyType, ValType, ColType extends Collection<ValType>> boolean saveCollection(@NotNull Map<KeyType, ColType> data, @NotNull final KeyType key, @NotNull final ValType value, @NotNull final String path);
+    /**
+     * Save element to inner {@link Map} in data {@link Map} and to storageFile to given path.
+     * @param data Data {@link Map} to save inner {@link Map} element to
+     * @param key Data {@link Map} key to inner {@link Map} to save element to
+     * @param mapKey Inner {@link Map} key to save element to
+     * @param value Value to save
+     * @param path Path to inner {@link Map} element in storageFile to save element to
+     * @param <KeyType> Data {@link Map} key type
+     * @param <MapKeyType> Inner {@link Map} key type
+     * @param <ValType> Inner {@link Map} value type
+     * @param <MapType> Type of inner {@link Map}
+     */
+    <KeyType, MapKeyType, ValType, MapType extends Map<MapKeyType, ValType>> void saveMap(@NotNull Map<KeyType, MapType> data, @NotNull final KeyType key, @NotNull final MapKeyType mapKey, @NotNull final ValType value, @Nullable final String path);
 
     /**
-     * Remove element from data {@link HashMap} and from storageFile from given path.
+     * Remove element from data {@link Map} and from storageFile from given path.
      * <br>Set path as null to remove whole key from storageFile.
      * <br>Set data as null to remove value only from storageFile.
-     * @param data {@link HashMap} to remove element from
-     * @param key {@link HashMap} key to remove element from
+     * @param data Data {@link Map} to remove element from
+     * @param key Data {@link Map} key to remove element from
      * @param path Path in storageFile to remove element from
      * @return True/False if remove succeed/failed
-     * @param <KeyType> Data {@link HashMap} key type
-     * @param <ValType> Data {@link HashMap} value type
+     * @param <KeyType> Data {@link Map} key type
+     * @param <ValType> Data {@link Map} value type
      */
-    <KeyType, ValType> boolean remove(@Nullable HashMap<KeyType, ValType> data, @NotNull final KeyType key, @Nullable final String path);
-
+    <KeyType, ValType> boolean remove(@Nullable Map<KeyType, ValType> data, @NotNull final KeyType key, @Nullable final String path);
     /**
-     * Remove element from {@link Collection} in data {@link HashMap} and from storageFile from given path.
-     * @param data {@link HashMap} to remove {@link Collection} element from
-     * @param key {@link HashMap} key to {@link Collection} to remove element from
+     * Remove element from {@link Collection} in data {@link Map} and from storageFile from given path.
+     * @param data Data {@link Map} to remove {@link Collection} element from
+     * @param key Data {@link Map} key to {@link Collection} to remove element from
      * @param value Value to remove
      * @param path Path to {@link com.google.gson.JsonArray} in storageFile to remove element from
      * @return True/False if remove succeed/failed
-     * @param <KeyType> Data {@link HashMap} key type
+     * @param <KeyType> Data {@link Map} key type
      * @param <ValType> {@link Collection} value type
      * @param <ColType> Type of {@link Collection}
      */
-    <KeyType, ValType, ColType extends Collection<ValType>> boolean removeCollection(@NotNull HashMap<KeyType, ColType> data, @NotNull final KeyType key, @NotNull final ValType value, @NotNull final String path);
+    <KeyType, ValType, ColType extends Collection<ValType>> boolean removeCollection(@NotNull Map<KeyType, ColType> data, @NotNull final KeyType key, @NotNull final ValType value, @NotNull final String path);
+    /**
+     * Remove element from inner {@link Map} in data {@link Map} and from storageFile from given path.
+     * <br>Set path as null to remove element by mapKey
+     * @param data Data {@link Map} to remove inner {@link Map} element from
+     * @param key Data {@link Map} key to inner {@link Map} to remove element from
+     * @param mapKey Inner {@link Map} key to remove
+     * @param path Path to inner {@link Map} element in storageFile to remove
+     * @return True/False if remove succeed/failed
+     * @param <KeyType> Data {@link Map} key type
+     * @param <MapKeyType> Inner {@link Map} key type
+     * @param <ValType> Inner {@link Map} value type
+     * @param <MapType> Type of inner {@link Map}
+     */
+    <KeyType, MapKeyType, ValType, MapType extends Map<MapKeyType, ValType>> boolean removeMap(@NotNull Map<KeyType, MapType> data, @NotNull final KeyType key, @NotNull final MapKeyType mapKey, @Nullable final String path);
 
     /**
-     * Clear {@link Collection} in data {@link HashMap} and in storageFile in given path.
-     * @param data {@link HashMap} to clear {@link Collection} from
-     * @param key {@link HashMap} key to {@link Collection} to clear
+     * Clear {@link Collection} in data {@link Map} and in storageFile in given path.
+     * @param data Data {@link Map} to clear {@link Collection} from
+     * @param key Data {@link Map} key to {@link Collection} to clear
      * @param path Path to {@link com.google.gson.JsonArray} in storageFile to clear
      * @return True/False if clear succeed/failed
-     * @param <KeyType> Data {@link HashMap} key type
+     * @param <KeyType> Data {@link Map} key type
      * @param <ValType> {@link Collection} value type
      * @param <ColType> Type of {@link Collection}
      */
-    <KeyType, ValType, ColType extends Collection<ValType>> boolean clearCollection(@NotNull HashMap<KeyType, ColType> data, @NotNull final KeyType key, @NotNull final String path);
+    <KeyType, ValType, ColType extends Collection<ValType>> boolean clearCollection(@NotNull Map<KeyType, ColType> data, @NotNull final KeyType key, @NotNull final String path);
+    /**
+     * Clear inner {@link Map} in data {@link Map} and in storageFile.
+     * @param data Data {@link Map} to clear inner {@link Map} from
+     * @param key Data {@link Map} key to inner {@link Map} to clear
+     * @param mapKeyType Inner {@link Map} key type
+     * @return True/False if clear succeed/failed
+     * @param <KeyType> Data {@link Map} key type
+     * @param <MapKeyType> Inner {@link Map} key type
+     * @param <ValType> Inner {@link Map} value type
+     * @param <MapType> Type of inner {@link Map}
+     */
+    <KeyType, MapKeyType, ValType, MapType extends Map<MapKeyType, ValType>> boolean clearMap(@NotNull Map<KeyType, MapType> data, @NotNull final KeyType key, @NotNull final Class<MapKeyType> mapKeyType);
 }
