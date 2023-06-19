878de9fb44ca61ff6d0a4001c78ec7116a4b4881
Wyne <officialfitcher@gmail.com>
2023-06-19 18:32:47 +0500

    Added MiniMessageChatMessageFieldType

--- config/src/main/java/me/wyne/wutils/config/configFieldTypes/MiniMessageChatMessageFieldType.java
+++ config/src/main/java/me/wyne/wutils/config/configFieldTypes/MiniMessageChatMessageFieldType.java
@@ -1,18 +1,19 @@
 package me.wyne.wutils.config.configFieldTypes;
 
+import me.wyne.wutils.chatMessages.ChatMessage;
 import me.wyne.wutils.config.ConfigParameter;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.jetbrains.annotations.NotNull;
 
-public class MiniMessageComponentFieldType implements ConfigParameter {
+public class MiniMessageChatMessageFieldType implements ConfigParameter {
 
     /**
-     * @return MiniMessage deserialized string as {@link net.kyori.adventure.text.Component}
+     * @return {@link MiniMessage} deserialized string as {@link ChatMessage}
      */
     @Override
     public Object getValue(@NotNull FileConfiguration config, @NotNull String path) {
-        return MiniMessage.miniMessage().deserialize(config.getString(path));
+        return new ChatMessage(MiniMessage.miniMessage().deserialize(config.getString(path)));
     }
 
 }
