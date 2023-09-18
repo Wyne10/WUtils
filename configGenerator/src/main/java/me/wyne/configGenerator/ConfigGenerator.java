63e069ee79f42fd4be6c2eb377d6699ab843efdb
Wyne <officialfitcher@gmail.com>
2023-09-18 15:48:09 +0500

    Add configGenerator module

---
+++ configGenerator/src/main/java/me/wyne/configGenerator/ConfigGenerator.java
@@ -0,0 +1,131 @@
+package me.wyne.configGenerator;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.jetbrains.annotations.Contract;
+
+import java.io.*;
+import java.lang.reflect.Field;
+
+public class ConfigGenerator {
+
+    public final static ConfigGenerator global = new ConfigGenerator();
+
+    private StringBuilder generatedText = new StringBuilder();
+
+    @Contract("_ -> this")
+    public ConfigGenerator writeString(String string)
+    {
+        generatedText.append(string);
+        whitespace();
+        return this;
+    }
+
+    @Contract("_, _ -> this")
+    public ConfigGenerator writeValue(String path, Object value)
+    {
+        generatedText.append(path);
+        generatedText.append(": ");
+        generatedText.append(value.toString());
+        whitespace();
+        return this;
+    }
+
+    @Contract("_, _, _ -> this")
+    public ConfigGenerator writeValue(String path, Object value, String comment)
+    {
+        generatedText.append(comment);
+        whitespace();
+        writeValue(path, value);
+        return this;
+    }
+
+    @Contract("-> this")
+    public ConfigGenerator whitespace() {
+        generatedText.append("\n");
+        return this;
+    }
+
+    @Contract("-> this")
+    public ConfigGenerator clear() {
+        generatedText = new StringBuilder();
+        return this;
+    }
+
+    public static void writeConfigObject(Object object)
+    {
+        for(Field field : object.getClass().getDeclaredFields())
+        {
+            if (field.isAnnotationPresent(ConfigGeneratorField.class))
+            {
+                field.setAccessible(true);
+                ConfigGeneratorField fieldAnnotation = field.getAnnotation(ConfigGeneratorField.class);
+                String path = fieldAnnotation.path().isEmpty() ? field.getName() : fieldAnnotation.path();
+                Object value = null;
+                try {
+                    value = fieldAnnotation.value().isEmpty() ? field.get(object) : fieldAnnotation.value();
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+
+                if (!fieldAnnotation.comment().isEmpty())
+                    global.writeValue(path, value, fieldAnnotation.comment());
+                else
+                    global.writeValue(path, value);
+            }
+        }
+    }
+
+    public void generate(File configFile)
+    {
+        FileConfiguration config = YamlConfiguration.loadConfiguration(configFile);
+
+        if (config.getBoolean("config-generator") == true)
+            return;
+        else if (!config.contains("config-generator"))
+            writeInitialData(configFile, config);
+        else if (config.getBoolean("config-generator") == false)
+        {
+            overrideData(configFile);
+            writeInitialData(configFile, config);
+        }
+    }
+
+    private void writeInitialData(File configFile, FileConfiguration config)
+    {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(configFile, true);
+             PrintStream printStream = new PrintStream(fileOutputStream)) {
+            if (!config.contains("config-generator"))
+            {
+                printStream.println("\n\n# This value is used as an indicator of successful generation of the config.\n# Set the value to false to regenerate part of config that was generated automatically");
+                printStream.println("config-generator: true\n");
+            }
+            printStream.println("# THIS PART OF CONFIG WAS GENERATED AUTOMATICALLY\n");
+            printStream.print(generatedText.toString());
+            printStream.println("\n# THIS PART OF CONFIG WAS GENERATED AUTOMATICALLY");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void overrideData(File configFile)
+    {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(configFile))) {
+            StringBuilder fileContent = new StringBuilder();
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.equalsIgnoreCase("config-generator: false"))
+                    break;
+                fileContent.append(line).append(System.lineSeparator());
+            }
+            bufferedReader.close();
+            fileContent.append("config-generator: true\n\n");
+            BufferedWriter writer = new BufferedWriter(new FileWriter(configFile));
+            writer.write(fileContent.toString());
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
