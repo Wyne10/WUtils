01ed45d534dc9a47c1e8d9e3f2238e469133f7a5
Wyne <izmodenov1997@gmail.com>
2024-10-02 20:19:25 +0500

    Add MaterialConfigurable

---
+++ configurables/src/main/java/me/wyne/wutils/config/configurables/MaterialConfigurable.java
@@ -0,0 +1,30 @@
+package me.wyne.wutils.config.configurables;
+
+import me.wyne.wutils.config.ConfigEntry;
+import me.wyne.wutils.config.configurable.Configurable;
+import org.bukkit.Material;
+
+public class MaterialConfigurable implements Configurable {
+
+    private Material material;
+
+    public MaterialConfigurable(Material material)
+    {
+        this.material = material;
+    }
+
+    @Override
+    public String toConfig(ConfigEntry configEntry) {
+        return material.toString();
+    }
+
+    @Override
+    public void fromConfig(Object configObject) {
+        this.material = Material.matchMaterial((String)configObject);
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+}
+
