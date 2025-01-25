b4e8d7ef33ca3bb25f0f9060eb77413497ee3178
Wyne10 <izmodenov1997@gmail.com>
2025-01-25 10:27:52 +0500

    Add extra placeholder methods

--- i18n/src/main/java/me/wyne/wutils/i18n/language/replacement/Placeholder.java
+++ i18n/src/main/java/me/wyne/wutils/i18n/language/replacement/Placeholder.java
@@ -1,5 +1,7 @@
 package me.wyne.wutils.i18n.language.replacement;
 
+import me.wyne.wutils.i18n.I18n;
+import me.wyne.wutils.i18n.language.interpretation.ComponentInterpreter;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
@@ -9,27 +11,37 @@ public class Placeholder {
 
     public static TextReplacement replace(String key, String value)
     {
-        return (string) -> string.replaceAll("<"+key+">", value);
+        return string -> string.replaceAll("<"+key+">", value);
+    }
+    
+    public static TextReplacement replace(String key, Component value)
+    {
+        return string -> string.replaceAll("<"+key+">", I18n.global.component().toString(value));
+    }
+
+    public static TextReplacement replace(String key, Component value, ComponentInterpreter interpreter)
+    {
+        return string -> string.replaceAll("<"+key+">", interpreter.toString(value));
     }
 
     public static TextReplacement plain(String key, Component value)
     {
-        return (string) -> string.replaceAll("<"+key+">", PlainTextComponentSerializer.plainText().serialize(value));
+        return string -> string.replaceAll("<"+key+">", PlainTextComponentSerializer.plainText().serialize(value));
     }
 
     public static TextReplacement legacy(String key, Component value)
     {
-        return (string) -> string.replaceAll("<"+key+">", LegacyComponentSerializer.legacyAmpersand().serialize(value));
+        return string -> string.replaceAll("<"+key+">", LegacyComponentSerializer.legacyAmpersand().serialize(value));
     }
 
     public static TextReplacement miniMessage(String key, Component value)
     {
-        return (string) -> string.replaceAll("<"+key+">", MiniMessage.miniMessage().serialize(value));
+        return string -> string.replaceAll("<"+key+">", MiniMessage.miniMessage().serialize(value));
     }
 
     public static TextReplacement regex(String regex, String value)
     {
-        return (string) -> string.replaceAll(regex, value);
+        return string -> string.replaceAll(regex, value);
     }
 
 }
