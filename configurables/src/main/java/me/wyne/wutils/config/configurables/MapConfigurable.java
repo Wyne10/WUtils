f6aa574cd2e41b8618f9c0f4cb6356c44a964321
Wyne10 <izmodenov1997@gmail.com>
2025-04-10 21:31:07 +0500

    Add map configurable config object constructor

--- configurables/src/main/java/me/wyne/wutils/config/configurables/MapConfigurable.java
+++ configurables/src/main/java/me/wyne/wutils/config/configurables/MapConfigurable.java
@@ -4,6 +4,7 @@ import me.wyne.wutils.common.MapUtils;
 
 import java.util.Map;
 
+@SuppressWarnings("unchecked")
 public class MapConfigurable<V> extends GenericMapConfigurable<String, V> {
 
     public MapConfigurable(Map<String, V> map, MapUtils.MapFunction<String, V, String, String> valueMapper, MapUtils.MapFunction<String, Object, String, V> configMapper) {
@@ -18,9 +19,14 @@ public class MapConfigurable<V> extends GenericMapConfigurable<String, V> {
         super(valueMapper, configMapper);
     }
 
-    @SuppressWarnings("unchecked")
     public MapConfigurable(Map<String, V> map) {
         this(map,
+                entry -> MapUtils.entry(entry.getKey(), entry.getValue().toString()),
+                entry -> MapUtils.entry(entry.getKey(), (V) entry.getValue()));
+    }
+
+    public MapConfigurable(Object configObject) {
+        this(configObject,
                 entry -> MapUtils.entry(entry.getKey(), entry.getValue().toString()),
                 entry -> MapUtils.entry(entry.getKey(), (V) entry.getValue()));
     }
