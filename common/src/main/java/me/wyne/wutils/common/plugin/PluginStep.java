62f2f39593dc022b09fe07f28e60ce108930cd2b
Wyne10 <izmodenov1997@gmail.com>
2025-04-17 22:38:00 +0500

    Make step annotation applicable to types

--- common/src/main/java/me/wyne/wutils/common/plugin/PluginStep.java
+++ common/src/main/java/me/wyne/wutils/common/plugin/PluginStep.java
@@ -2,10 +2,19 @@ package me.wyne.wutils.common.plugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+@FunctionalInterface
 public interface PluginStep {
     void run(JavaPlugin plugin);
-    StepScope getScope();
+    default StepScope getScope() {
+        if (getClass().isAnnotationPresent(Step.class)) {
+            return getClass().getAnnotation(Step.class).scope();
+        }
+        return StepScope.ENABLE;
+    }
     default int getPriority() {
+        if (getClass().isAnnotationPresent(Step.class)) {
+            return getClass().getAnnotation(Step.class).priority();
+        }
         return 0;
     }
 }
