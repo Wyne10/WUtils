ecb14087c4eabe5dcd70b7db42c75ee2bb053e4b
Wyne10 <izmodenov1997@gmail.com>
2025-04-17 18:50:39 +0500

    Add method to create additive log4j logger

--- log/src/main/java/me/wyne/wutils/log/Log4jFactory.java
+++ log/src/main/java/me/wyne/wutils/log/Log4jFactory.java
@@ -10,6 +10,7 @@ import org.apache.logging.log4j.core.appender.rolling.TriggeringPolicy;
 import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.LoggerConfig;
 import org.apache.logging.log4j.core.layout.PatternLayout;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,6 +24,7 @@ public final class Log4jFactory {
     public static final String DEFAULT_FILE_MESSAGE_PATTERN = "[%d{HH:mm:ss} %level]: [%c{1}] %msg%n";
 
     private static final Map<Class<?>, Logger> existingLoggers = new HashMap<>();
+    private static final Logger log = LoggerFactory.getLogger(Log4jFactory.class);
 
     public static Logger createLogger(Class<?> clazz, String messagePattern, Level logLevel) {
         if (existingLoggers.containsKey(clazz))
@@ -224,6 +226,57 @@ public final class Log4jFactory {
         }
 
         return existingLoggers.get(clazz);
+    }
+
+    public static Logger createLogger(JavaPlugin plugin, String filePattern, Level logLevel, String logDirectory, Log fallback) {
+        if (existingLoggers.containsKey(plugin.getClass()))
+            return existingLoggers.get(plugin.getClass());
+
+        try {
+            LoggerContext context = (LoggerContext) LogManager.getContext(false);
+            Configuration config = context.getConfiguration();
+
+            String logFileName = logDirectory + "/" + LocalDate.now() + ".log";
+            String logFilePattern = logDirectory + "/%d{yyyy-MM-dd}.log";
+
+            PatternLayout fileLayout = PatternLayout.newBuilder()
+                    .withPattern(filePattern)
+                    .withConfiguration(config)
+                    .build();
+
+            TriggeringPolicy triggeringPolicy = TimeBasedTriggeringPolicy.newBuilder()
+                    .withInterval(1)
+                    .withModulate(true)
+                    .build();
+
+            DefaultRolloverStrategy rollingPolicy = DefaultRolloverStrategy.newBuilder()
+                    .withMax("7")
+                    .withConfig(config)
+                    .build();
+
+            RollingFileAppender rollingFileAppender = RollingFileAppender.newBuilder()
+                    .setName("RollingFile")
+                    .withFileName(logFileName)
+                    .withFilePattern(logFilePattern)
+                    .withPolicy(triggeringPolicy)
+                    .withStrategy(rollingPolicy)
+                    .setLayout(fileLayout)
+                    .setConfiguration(config)
+                    .build();
+            rollingFileAppender.start();
+            config.addAppender(rollingFileAppender);
+
+            LoggerConfig loggerConfig = new LoggerConfig(plugin.getLogger().getName(), logLevel.getLevel(), true);
+            loggerConfig.addAppender(rollingFileAppender, logLevel.getLevel(), null);
+            config.addLogger(plugin.getLogger().getName(), loggerConfig);
+            context.updateLoggers();
+
+            existingLoggers.putIfAbsent(plugin.getClass(), LoggerFactory.getLogger(plugin.getLogger().getName()));
+        } catch (NoSuchMethodError ignored) {
+            existingLoggers.putIfAbsent(plugin.getClass(), new JulLogger(fallback));
+        }
+
+        return existingLoggers.get(plugin.getClass());
     }
 
 }
