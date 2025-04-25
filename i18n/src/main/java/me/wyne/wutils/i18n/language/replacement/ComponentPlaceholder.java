f170147dab36a404c76dd40ae147178f0d4462d8
Wyne10 <izmodenov1997@gmail.com>
2025-04-25 20:41:39 +0500

    Add base component replacements

--- i18n/src/main/java/me/wyne/wutils/i18n/language/replacement/ComponentPlaceholder.java
+++ i18n/src/main/java/me/wyne/wutils/i18n/language/replacement/ComponentPlaceholder.java
@@ -1,6 +1,8 @@
 package me.wyne.wutils.i18n.language.replacement;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.bungeecord.BungeeComponentSerializer;
+import net.md_5.bungee.api.chat.BaseComponent;
 import org.intellij.lang.annotations.RegExp;
 
 import java.util.regex.Pattern;
@@ -17,6 +19,11 @@ public class ComponentPlaceholder {
         return component -> component.replaceText(builder -> builder.matchLiteral("<"+key+">").replacement(value));
     }
 
+    public static ComponentReplacement replace(String key, BaseComponent[] value)
+    {
+        return component -> component.replaceText(builder -> builder.matchLiteral("<"+key+">").replacement(BungeeComponentSerializer.get().deserialize(value)));
+    }
+
     public static ComponentReplacement regex(@RegExp String regex, String value)
     {
         return component -> component.replaceText(builder -> builder.match(regex).replacement(value));
@@ -27,6 +34,11 @@ public class ComponentPlaceholder {
         return component -> component.replaceText(builder -> builder.match(regex).replacement(value));
     }
 
+    public static ComponentReplacement regex(@RegExp String regex, BaseComponent[] value)
+    {
+        return component -> component.replaceText(builder -> builder.match(regex).replacement(BungeeComponentSerializer.get().deserialize(value)));
+    }
+
     public static ComponentReplacement regex(Pattern regex, String value)
     {
         return component -> component.replaceText(builder -> builder.match(regex).replacement(value));
@@ -35,6 +47,11 @@ public class ComponentPlaceholder {
     public static ComponentReplacement regex(Pattern regex, Component value)
     {
         return component -> component.replaceText(builder -> builder.match(regex).replacement(value));
+    }
+
+    public static ComponentReplacement regex(Pattern regex, BaseComponent[] value)
+    {
+        return component -> component.replaceText(builder -> builder.match(regex).replacement(BungeeComponentSerializer.get().deserialize(value)));
     }
     
 }
