daff9ac51930ad6020b553718c01ac5c2075a059
Wyne10 <izmodenov1997@gmail.com>
2025-05-26 00:16:39 +0500

    Add primitive configurable factory

--- configurables/src/main/java/me/wyne/wutils/config/configurables/attribute/PrimitiveConfigurableAttribute.java
+++ configurables/src/main/java/me/wyne/wutils/config/configurables/attribute/PrimitiveConfigurableAttribute.java
@@ -1,9 +1,18 @@
 package me.wyne.wutils.config.configurables.attribute;
 
+import org.bukkit.configuration.ConfigurationSection;
+
 public class PrimitiveConfigurableAttribute<V> extends AttributeBase<V> implements ConfigurableAttribute<V> {
 
     public PrimitiveConfigurableAttribute(String key, V value) {
         super(key, value);
+    }
+
+    public static final class Factory implements AttributeFactory {
+        @Override
+        public ConfigurableAttribute<?> create(String key, ConfigurationSection config) {
+            return new PrimitiveConfigurableAttribute<>(key, config.get(key));
+        }
     }
 
 }
