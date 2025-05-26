3b6bae9e120270fc6dfdbcc0eb547f7c0e0e2178
Wyne10 <izmodenov1997@gmail.com>
2025-05-26 22:07:56 +0500

    Add more meaningful name to replacement conversions

--- i18n/src/main/java/me/wyne/wutils/i18n/language/replacement/ComponentReplacement.java
+++ i18n/src/main/java/me/wyne/wutils/i18n/language/replacement/ComponentReplacement.java
@@ -4,7 +4,7 @@ import me.wyne.wutils.i18n.I18n;
 import net.kyori.adventure.text.Component;
 
 public interface ComponentReplacement extends Replacement<Component> {
-    default TextReplacement as() {
+    default TextReplacement asTextReplacement() {
         return string -> I18n.global.component().toString(replace(I18n.global.component().fromString(string)));
     }
 
--- i18n/src/main/java/me/wyne/wutils/i18n/language/replacement/TextReplacement.java
+++ i18n/src/main/java/me/wyne/wutils/i18n/language/replacement/TextReplacement.java
@@ -3,7 +3,7 @@ package me.wyne.wutils.i18n.language.replacement;
 import me.wyne.wutils.i18n.I18n;
 
 public interface TextReplacement extends Replacement<String> {
-    default ComponentReplacement as() {
+    default ComponentReplacement asComponentReplacement() {
         return component -> I18n.global.component().fromString(replace(I18n.global.component().toString(component)));
     }
 
