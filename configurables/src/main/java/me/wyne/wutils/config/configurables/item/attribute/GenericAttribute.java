bf7c541c83d9410c4b67202d29644b26ef6b82a6
Wyne10 <izmodenov1997@gmail.com>
2025-09-26 18:39:16 +0500

    Generic attribute bug fix

--- configurables/src/main/java/me/wyne/wutils/config/configurables/item/attribute/GenericAttribute.java
+++ configurables/src/main/java/me/wyne/wutils/config/configurables/item/attribute/GenericAttribute.java
@@ -51,7 +51,7 @@ public class GenericAttribute extends ConfigurableAttribute<GenericAttribute.Att
                                     section.getString("name", "Attribute"),
                                     section.getDouble("amount", 1.0),
                                     AttributeModifier.Operation.valueOf(section.getString("operation", "ADD_NUMBER")),
-                                    EquipmentSlot.valueOf(section.getString("slot", "HAND"))
+                                    section.contains("slot") ? EquipmentSlot.valueOf(section.getString("slot", "HAND")) : null
                             )
                     )
             );
@@ -70,7 +70,7 @@ public class GenericAttribute extends ConfigurableAttribute<GenericAttribute.Att
                                     "Attribute",
                                     Double.parseDouble(args.get(1, "1.0")),
                                     AttributeModifier.Operation.valueOf(args.get(2, "ADD_NUMBER")),
-                                    EquipmentSlot.valueOf(args.get(3, "HAND"))
+                                    args.size() == 4 ? EquipmentSlot.valueOf(args.get(3, "HAND")) : null
                             )
                     )
             );
