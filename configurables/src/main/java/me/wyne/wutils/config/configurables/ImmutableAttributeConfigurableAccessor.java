73d8f66959151d8e0b1929cf2a91d1529b5c53e6
Wyne10 <izmodenov1997@gmail.com>
2025-11-08 16:11:44 +0500

    Fix immutable attribute configurable accessor

--- configurables/src/main/java/me/wyne/wutils/config/configurables/ImmutableAttributeConfigurableAccessor.java
+++ configurables/src/main/java/me/wyne/wutils/config/configurables/ImmutableAttributeConfigurableAccessor.java
@@ -4,50 +4,66 @@ import me.wyne.wutils.config.configurables.attribute.Attribute;
 import me.wyne.wutils.config.configurables.attribute.AttributeContainer;
 import me.wyne.wutils.config.configurables.attribute.AttributeFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 
 @SuppressWarnings("unchecked")
 public class ImmutableAttributeConfigurableAccessor<T extends AttributeConfigurable> implements AttributeConfigurableAccessor<T> {
 
     private final T attributeConfigurable;
+    private final Constructor<? extends AttributeConfigurable> constructor;
 
     public ImmutableAttributeConfigurableAccessor(T attributeConfigurable) {
         this.attributeConfigurable = attributeConfigurable;
+        try {
+            this.constructor = attributeConfigurable.getClass().getConstructor(AttributeContainer.class);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException("Configurable must contain constructor with AttributeContainer parameter in order to use immutable attribute configurable accessor", e);
+        }
     }
 
     @Override
     public T ignore(String... ignore) {
-        return (T) new AttributeConfigurable(attributeConfigurable.getAttributeContainer().ignore(ignore));
+        return create(attributeConfigurable.getAttributeContainer().ignore(ignore));
     }
 
     @Override
     public T with(String key, AttributeFactory factory) {
-        return (T) new AttributeConfigurable(attributeConfigurable.getAttributeContainer().with(key, factory));
+        return create(attributeConfigurable.getAttributeContainer().with(key, factory));
     }
 
     @Override
     public T with(Map<String, AttributeFactory> keyMap) {
-        return (T) new AttributeConfigurable(attributeConfigurable.getAttributeContainer().with(keyMap));
+        return create(attributeConfigurable.getAttributeContainer().with(keyMap));
     }
 
     @Override
     public T with(Attribute<?> attribute) {
-        return (T) new AttributeConfigurable(attributeConfigurable.getAttributeContainer().with(attribute));
+        return create(attributeConfigurable.getAttributeContainer().with(attribute));
     }
 
     @Override
     public T with(AttributeContainer container) {
-        return (T) new AttributeConfigurable(attributeConfigurable.getAttributeContainer().with(container));
+        return create(attributeConfigurable.getAttributeContainer().with(container));
     }
 
     @Override
     public T copy(AttributeContainer container) {
-        return (T) new AttributeConfigurable(attributeConfigurable.getAttributeContainer().copy(container));
+        return create(attributeConfigurable.getAttributeContainer().copy(container));
     }
 
     @Override
     public T copy() {
-        return (T) new AttributeConfigurable(attributeConfigurable.getAttributeContainer().copy());
+        return create(attributeConfigurable.getAttributeContainer().copy());
+    }
+
+    private T create(AttributeContainer container) {
+        try {
+            return (T) constructor.newInstance(container);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException("Configurable must contain constructor with AttributeContainer parameter in order to use immutable attribute configurable accessor", e);
+        }
     }
 
 }
