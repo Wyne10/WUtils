1bc252b47d4b5437ef04a42daf0b52b68d02a3d8
Wyne10 <izmodenov1997@gmail.com>
2025-11-15 18:45:50 +0500

    Make promised tasks and observable tasks standalone classes

--- common/src/main/java/me/wyne/wutils/common/scheduler/ObservableTask.java
+++ common/src/main/java/me/wyne/wutils/common/scheduler/ObservableTask.java
@@ -3,17 +3,19 @@ package me.wyne.wutils.common.scheduler;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitTask;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.LinkedList;
 import java.util.List;
 
-public class ObservableScheduler {
+public class ObservableTask {
 
     private final Plugin plugin;
     private final List<Runnable> subscribers = new LinkedList<>();
+
     private BukkitTask task;
 
-    public ObservableScheduler(Plugin plugin) {
+    public ObservableTask(Plugin plugin) {
         this.plugin = plugin;
     }
 
@@ -26,22 +28,22 @@ public class ObservableScheduler {
     }
 
     public void runTaskLater(long delay) {
-        if (task != null && !task.isCancelled()) task.cancel();
+        cancel();
         task = Bukkit.getScheduler().runTaskLater(plugin, this::run, delay);
     }
 
     public void runTaskLaterAsynchronously(long delay) {
-        if (task != null && !task.isCancelled()) task.cancel();
+        cancel();
         task = Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, this::run, delay);
     }
 
     public void runTaskTimer(long delay, long period) {
-        if (task != null && !task.isCancelled()) task.cancel();
+        cancel();
         task = Bukkit.getScheduler().runTaskTimer(plugin, this::run, delay, period);
     }
 
     public void runTaskTimerAsynchronously(long delay, long period) {
-        if (task != null && !task.isCancelled()) task.cancel();
+        cancel();
         task = Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, this::run, delay, period);
     }
 
@@ -50,7 +52,17 @@ public class ObservableScheduler {
     }
 
     public void cancel() {
-        if (task != null && !task.isCancelled()) task.cancel();
+        if (task == null || task.isCancelled()) return;
+        task.cancel();
+    }
+
+    @Nullable
+    public BukkitTask getTask() {
+        return task;
+    }
+
+    public List<Runnable> getSubscribers() {
+        return subscribers;
     }
 
 }
--- common/src/main/java/me/wyne/wutils/common/scheduler/PromisedTask.java
+++ common/src/main/java/me/wyne/wutils/common/scheduler/PromisedTask.java
@@ -1,74 +1,52 @@
 package me.wyne.wutils.common.scheduler;
 
 import org.bukkit.Bukkit;
-import org.bukkit.event.Event;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-
-public class PromisedTask<T extends Event> {
+public class PromisedTask {
 
     private final Plugin plugin;
     private final Runnable runnable;
-    private final Consumer<T> promise;
-    private final Predicate<T> condition;
-    private final long delay;
+    private final Runnable promise;
 
     private BukkitTask task;
 
-    public PromisedTask(Plugin plugin, Runnable runnable, Consumer<T> promise, Predicate<T> condition, long delay) {
+    public PromisedTask(Plugin plugin, Runnable runnable, Runnable promise) {
         this.plugin = plugin;
         this.runnable = runnable;
         this.promise = promise;
-        this.condition = condition;
-        this.delay = delay;
     }
 
-    public PromisedTask(Plugin plugin, Runnable runnable, Consumer<T> promise, long delay) {
-        this(plugin, runnable, promise, event -> true, delay);
+    public PromisedTask(Plugin plugin, Runnable runnable) {
+        this(plugin, runnable, runnable);
     }
 
-    public PromisedTask(Plugin plugin, Runnable runnable, Predicate<T> condition, long delay) {
-        this(plugin, runnable, event -> runnable.run(), condition, delay);
-    }
-
-    public PromisedTask(Plugin plugin, Runnable runnable, long delay) {
-        this(plugin, runnable, event -> runnable.run(), event -> true, delay);
-    }
-
-    public BukkitTask start() {
+    public void runTaskLater(long delay) {
+        cancel();
         task = Bukkit.getScheduler().runTaskLater(plugin, runnable, delay);
-        return task;
     }
 
-    public void cancel(Event event) {
+    public void runTaskLaterAsynchronously(long delay) {
+        cancel();
+        task = Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, runnable, delay);
+    }
+
+    public void runTaskTimer(long delay, long period) {
+        cancel();
+        task = Bukkit.getScheduler().runTaskTimer(plugin, runnable, delay, period);
+    }
+
+    public void runTaskTimerAsynchronously(long delay, long period) {
+        cancel();
+        task = Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, runnable, delay, period);
+    }
+
+    public void cancel() {
         if (task == null || task.isCancelled()) return;
-        if (!condition.test((T) event)) return;
         task.cancel();
-        promise.accept((T) event);
-    }
-
-    public Plugin getPlugin() {
-        return plugin;
-    }
-
-    public Runnable getRunnable() {
-        return runnable;
-    }
-
-    public Consumer<T> getPromise() {
-        return promise;
-    }
-
-    public Predicate<T> getCondition() {
-        return condition;
-    }
-
-    public long getDelay() {
-        return delay;
+        promise.run();
     }
 
     @Nullable
