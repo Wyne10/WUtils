84d0190563f03bda579107197e250e935f86db47
Wyne10 <izmodenov1997@gmail.com>
2025-12-23 02:18:09 +0500

    Add getAffectedItems method to inventory utils

--- common/src/main/java/me/wyne/wutils/common/inventory/InventoryUtils.java
+++ common/src/main/java/me/wyne/wutils/common/inventory/InventoryUtils.java
@@ -1,8 +1,15 @@
 package me.wyne.wutils.common.inventory;
 
+import me.wyne.wutils.common.item.ItemUtils;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public final class InventoryUtils {
 
@@ -18,5 +25,22 @@ public final class InventoryUtils {
         var exceed = player.getInventory().addItem(items);
         exceed.values().forEach(item -> player.getLocation().getWorld().dropItem(player.getLocation(), item));
     }
+
+    public static List<ItemStack> getAffectedItems(InventoryClickEvent event) {
+        var result = new ArrayList<ItemStack>();
+        addNonEmpty(result, event.getCurrentItem());
+        if (event.getClick().isKeyboardClick()) {
+            addNonEmpty(result, event.getWhoClicked().getInventory().getItem(event.getHotbarButton()));
+        } else if (event.getClick() == ClickType.SWAP_OFFHAND) {
+            addNonEmpty(result, event.getWhoClicked().getInventory().getItemInOffHand());
+        }
+        return result;
+    }
+
+    private static void addNonEmpty(List<ItemStack> list, ItemStack item) {
+        if (ItemUtils.isNotNullOrAir(item))
+            list.add(item);
+    }
+
 
 }
