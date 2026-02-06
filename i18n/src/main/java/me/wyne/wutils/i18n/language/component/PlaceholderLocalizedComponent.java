6ed08a84cd92a289ab8764182a743041a918eaa5
Wyne10 <izmodenov1997@gmail.com>
2026-02-06 20:07:42 +0500

    Override replace methods in placeholdered variants of localized components

--- i18n/src/main/java/me/wyne/wutils/i18n/language/component/PlaceholderLocalizedComponent.java
+++ i18n/src/main/java/me/wyne/wutils/i18n/language/component/PlaceholderLocalizedComponent.java
@@ -2,6 +2,7 @@ package me.wyne.wutils.i18n.language.component;
 
 import me.wyne.wutils.i18n.language.Language;
 import me.wyne.wutils.i18n.language.interpretation.ComponentInterpreter;
+import me.wyne.wutils.i18n.language.replacement.ComponentReplacement;
 import net.kyori.adventure.text.Component;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
@@ -15,6 +16,14 @@ public class PlaceholderLocalizedComponent extends LocalizedComponent implements
     public PlaceholderLocalizedComponent(ComponentInterpreter interpreter, Language language, String path, Component component, ComponentAudiences audiences, @Nullable OfflinePlayer player) {
         super(interpreter, language, path, component, audiences);
         this.player = player;
+    }
+
+    @Override
+    public PlaceholderLocalizedComponent replace(ComponentReplacement... componentReplacements) {
+        Component result = Component.empty().append(get());
+        for (ComponentReplacement replacement : componentReplacements)
+            result = replacement.replace(result);
+        return new PlaceholderLocalizedComponent(getInterpreter(), getLanguage(), getPath(), result, getAudiences(), player);
     }
 
     @Override
--- i18n/src/main/java/me/wyne/wutils/i18n/language/component/PlaceholderLocalizedString.java
+++ i18n/src/main/java/me/wyne/wutils/i18n/language/component/PlaceholderLocalizedString.java
@@ -1,7 +1,9 @@
 package me.wyne.wutils.i18n.language.component;
 
+import me.wyne.wutils.i18n.I18n;
 import me.wyne.wutils.i18n.language.Language;
 import me.wyne.wutils.i18n.language.interpretation.StringInterpreter;
+import me.wyne.wutils.i18n.language.replacement.TextReplacement;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
@@ -14,6 +16,11 @@ public class PlaceholderLocalizedString extends LocalizedString implements Place
     public PlaceholderLocalizedString(StringInterpreter interpreter, Language language, String path, String string, @Nullable OfflinePlayer player) {
         super(interpreter, language, path, string);
         this.player = player;
+    }
+
+    @Override
+    public PlaceholderLocalizedString replace(TextReplacement... textReplacements) {
+        return new PlaceholderLocalizedString(getInterpreter(), getLanguage(), getPath(), I18n.applyTextReplacements(get(), textReplacements), player);
     }
 
     @Override
