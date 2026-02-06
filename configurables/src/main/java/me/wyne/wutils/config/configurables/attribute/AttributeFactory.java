44a78bc8445cea121a3a7c7dfd5e59358de0edd2
Wyne10 <izmodenov1997@gmail.com>
2026-02-06 19:23:41 +0500

    Make GenericFactory, AttributeFactory dunctional interfaces

--- configurables/src/main/java/me/wyne/wutils/config/configurables/attribute/AttributeFactory.java
+++ configurables/src/main/java/me/wyne/wutils/config/configurables/attribute/AttributeFactory.java
@@ -1,3 +1,4 @@
 package me.wyne.wutils.config.configurables.attribute;
 
+@FunctionalInterface
 public interface AttributeFactory<T extends Attribute<?>> extends GenericFactory<T> {}
--- configurables/src/main/java/me/wyne/wutils/config/configurables/attribute/GenericFactory.java
+++ configurables/src/main/java/me/wyne/wutils/config/configurables/attribute/GenericFactory.java
@@ -2,6 +2,7 @@ package me.wyne.wutils.config.configurables.attribute;
 
 import org.bukkit.configuration.ConfigurationSection;
 
+@FunctionalInterface
 public interface GenericFactory<T> {
     T create(String key, ConfigurationSection config);
 }
