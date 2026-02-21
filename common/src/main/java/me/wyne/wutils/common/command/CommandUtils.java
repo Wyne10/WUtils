37b9acddeb9cac448fc41cb97d67b47b9e80b440
Wyne10 <izmodenov1997@gmail.com>
2026-02-21 18:28:22 +0500

    Add offline player getter in command utils

--- common/src/main/java/me/wyne/wutils/common/command/CommandUtils.java
+++ common/src/main/java/me/wyne/wutils/common/command/CommandUtils.java
@@ -1,9 +1,12 @@
 package me.wyne.wutils.common.command;
 
 import dev.jorel.commandapi.arguments.*;
+import dev.jorel.commandapi.executors.CommandArguments;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -20,6 +23,17 @@ public final class CommandUtils {
         return new StringArgument(nodeName)
                 .replaceSuggestions(ArgumentSuggestions.stringCollection(info ->
                         Arrays.stream(Bukkit.getOfflinePlayers()).map(OfflinePlayer::getName).toList()));
+    }
+
+    @Nullable
+    public static OfflinePlayer getOfflinePlayer(CommandArguments args, String nodeName, CommandSender sender) {
+        var playerName = args.getOptionalByClass(nodeName, String.class);
+        if (playerName.isEmpty()) return null;
+        var playerUuid = Bukkit.getPlayerUniqueId(playerName.get());
+        if (playerUuid == null) return null;
+        var player = Bukkit.getOfflinePlayer(playerUuid);
+        if (!player.hasPlayedBefore()) return null;
+        return player;
     }
 
 }
--- commonKt/src/main/kotlin/me/wyne/wutils/common/kotlin/command/CommandUtils.kt
+++ commonKt/src/main/kotlin/me/wyne/wutils/common/kotlin/command/CommandUtils.kt
@@ -3,6 +3,9 @@ package me.wyne.wutils.common.kotlin.command
 import dev.jorel.commandapi.IStringTooltip
 import dev.jorel.commandapi.arguments.Argument
 import dev.jorel.commandapi.arguments.ArgumentSuggestions
+import dev.jorel.commandapi.executors.CommandArguments
+import me.wyne.wutils.common.command.CommandUtils
+import org.bukkit.command.CommandSender
 
 fun <T : Argument<*>> T.suggest(vararg strings: String): T =
     replaceSuggestions(ArgumentSuggestions.strings(*strings)) as T
@@ -15,3 +18,6 @@ fun <T : Argument<*>> T.tooltip(vararg tooltips: IStringTooltip): T =
 
 fun <T : Argument<*>> T.tooltip(tooltips: Collection<IStringTooltip>): T =
     replaceSuggestions(ArgumentSuggestions.stringsWithTooltips(tooltips)) as T
+
+fun CommandArguments.getOfflinePlayer(nodeName: String, sender: CommandSender) =
+    CommandUtils.getOfflinePlayer(this, nodeName, sender)
